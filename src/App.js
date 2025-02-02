33ac8863008d64ed55c35bb8389b8ba0948584a8
Jonathan <jonathanbgjo@gmail.com>
2025-02-02 10:21:02 -0800

    add map

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Tenants from './Tenants';
 import Contact from './Contact';
 import ImageGallery from './Gallery';
 import Banner from './Banner';
-
+import MapComponent from './Map';
 function App() {
   return (
     <div className="App">
       <Banner />
+      <MapComponent />
       <Header />
       <ImageGallery />
       <Location />
---
+++ src/Map.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect, onLoad } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '400px',
+};
+const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+const center = {
+  lat: 47.9101,
+  lng: -122.224,
+};
+const Map = () => {
+  const [mapLoaded, setMapLoaded] = useState(false);
+
+  useEffect(() => {
+    // This ensures that the Google Maps API is loaded
+    const loadGoogleMaps = () => {
+      setMapLoaded(true);
+    };
+
+    // You could also load the Google Maps API manually here if needed
+    loadGoogleMaps();
+  }, []);
+
+  return (
+    <LoadScript
+    googleMapsApiKey={apiKey}
+    onLoad={() => setMapLoaded(true)}
+    onError={(error) => console.error('Error loading Google Maps API:', error)}
+    >
+      {mapLoaded ? (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={17}
+          onLoad={onLoad}
+        >
+          {/* Add a Marker */}
+          <Marker position={center} />
+        </GoogleMap>
+      ) : (
+        <p>Loading Map...</p>
+      )}
+    </LoadScript>
+  );
+};
+
+export default Map;
